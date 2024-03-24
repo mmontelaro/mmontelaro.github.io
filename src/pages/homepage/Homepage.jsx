942ac3b5ebf15771c2535f0af79b859d1d6cacc0
mmontelaro <135753734+mmontelaro@users.noreply.github.com>
2024-03-23 17:52:40 -0700

    Profile

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,39 +1,55 @@
 import NavBar from "../../NavBar";
-
+import "./Homepage.css"
 
 export default function Homepage() {
 
-    // const rc = (r) => {return (Math.random()*255)*r}
-    // const ra = () => {return Math.random()*0.1}
-    // const lg = () => {return "linear-gradient(" + Math.random()*365 + "deg, rgba(" + rc(0.7) + "," + rc(1) + "," + rc(1) + "," + ra() + ")," +
-    // "rgba(" + rc(0.7) + "," + rc(1) + "," + rc(1) + "," + ra() + "))"}
-    // const lgs = () => {
-    //     let res = lg();
-    //     const max = Math.random()*3
-    //     for (let i=0; i<max; i++) {
-    //         res += ", " + lg()
-    //     }
-    //     return res;
-    // }
+    const bgStyle = {display: "flex", flexDirection: "column", overflowY: "hidden", height: "auto"}
 
-    const bgStyle = {height: "100vh", display: "flex", flexDirection: "column"}
+    const profileStyle = {display: "flex", justifyContent: "space-around"}
+
+    const aboutStyle = {display: "flex", flexDirection: "column"}
+
+    const outerNameStyle = {fontFamily: "Georgia", fontSize: "4.5vmin", margin: "5vmin 0 2vmin 0", display: "flex"}
+
+    const innerNameStyle = {display: "inline-block", borderBottom: "1px solid black"}
+
+    const pStyle = {fontFamily: "Georgia", fontSize: "3vmin", maxWidth: "30vw", marginTop: "0"}
 
     const bottomBar = {display: "flex", justifyContent: "space-around", marginTop: "auto"}
 
-    const iconStyle = {height: "5vh"}
-
-    return (<div style={bgStyle}>
+    return (<div style={{display: "flex", flexDirection: "column", height: "100vh"}}>
         <NavBar />
-        Site Under Construction
+        <div style={bgStyle}>
+            Site Under Construction
+            <div style={profileStyle}>
+                <div style={aboutStyle}>
+                    <div style={outerNameStyle}>
+                        <div style={innerNameStyle}>
+                            Matthew Montelaro
+                        </div>
+                        <div style={{borderBottom: "1px solid black", width: "4vw"}} />
+                    </div>
+                    <p style={pStyle}> Hello! I'm Matthew Montelaro, a soon to graduate Computer Science major at UW Seattle!  </p>
+                </div>
+                <div className={"home__img"}/>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
         <div style={bottomBar}>
             <a href={"http://www.linkedin.com/in/matthew-montelaro-742a43296"}>
-                <img style={iconStyle} src={"linkedin.png"} alt={"LinkedIn"}/>
+                <img className={"icon"} src={"linkedin.png"} alt={"LinkedIn"}/>
             </a>
             <a href={"https://github.com/mmontelaro"}>
-                <img style={iconStyle} src={"github.png"} alt={"Github"}/>
+                <img className={"icon"} src={"github.png"} alt={"Github"}/>
+            </a>
+            <a href={"/resumeForWebsite.pdf"}>
+                <img className={"icon"} src={"resumeicon.png"} alt={"Resume"}/>
             </a>
             <a href={"http://www.youtube.com/@puggietaur"}>
-                <img style={iconStyle} src={"youtube.png"} alt={"Youtube"}/>
+                <img className={"icon"} src={"youtube.png"} alt={"Youtube"}/>
             </a>
         </div>
     </div>)
